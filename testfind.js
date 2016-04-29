9e5c9a98a6bbbecc85e8800f1abaca2a68c3cd63
durantjay <durantjay1@gmail.com>
2016-04-29 14:16:13 -0700

    Add testfind document

---
+++ testfind.js
@@ -0,0 +1,24 @@
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var limit = parseInt(process.argv[2]);
+
+mongo.connect(url, (err,db) => {
+	if(err){
+		console.log('error in connecting to the database')
+	}
+	else if(db){
+		var parrots = db.collection('parrots');
+		//console.log(parrots)
+		parrots.find({age: {$gt: limit}}).toArray( (err,docs) => {
+			if(err){
+				console.log('error at query')
+			} else{
+				console.log(docs)
+				db.close();
+			}
+			
+		})
+		
+	}
+
+})
